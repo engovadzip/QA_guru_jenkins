bddc17382aae917ca2450bf2fa7285cd73144b89
Engo Vadzip <abzxcvvcxzba@yandex.ru>
2023-10-31 16:11:57 +0300

    Initial version

---
+++ pages/registration_page_mid.py
@@ -0,0 +1,82 @@
+from selene import browser, be, have
+import os
+
+
+class RegistrationPage():
+    def open_form(self):
+        browser.open('/automation-practice-form')
+
+    def fill_first_name(self, name):
+        browser.element('//input[@placeholder="First Name"]').send_keys(name)
+
+    def fill_last_name(self, last_name):
+        browser.element('//input[@placeholder="Last Name"]').send_keys(last_name)
+
+    def fill_email(self, email):
+        browser.element('#userEmail').send_keys(email)
+
+    def select_gender(self, gender):
+        gender = gender.capitalize()
+        browser.element(f'//label[text() = "{gender}"]').click()
+
+    def fill_phone_number(self, phone):
+        browser.element('//input[@placeholder="Mobile Number"]').send_keys(phone)
+
+    def select_date_of_birth(self, month, day, year):
+        month = month.capitalize()
+        browser.element('#dateOfBirthInput').click()
+        browser.element('//select[@class="react-datepicker__month-select"]').click()
+        browser.element(f'//*[text()="{month}"]').click()
+
+        browser.element('//select[@class="react-datepicker__year-select"]').click()
+        browser.element(f'//*[text()={year}]').click()
+        browser.element(f'//div[text()={day}]').click()
+
+        browser.element('//input[@placeholder="First Name"]').click()
+
+    def select_subject(self, subject):
+        browser.element('#subjectsInput').type(f'{subject}').press_enter()
+
+    def select_hobbies(self):
+        browser.element('//label[@for="hobbies-checkbox-1"]').click()
+        browser.element('//label[@for="hobbies-checkbox-3"]').click()
+
+    def upload_picture(self):
+        current_dir = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(current_dir, 'picture.jpg')
+        browser.element('#uploadPicture').should(be.blank).send_keys(file_path)
+
+    def fill_address(self, address):
+        browser.element('//textarea[@placeholder="Current Address"]').send_keys(f'{address}')
+
+    def select_state(self, state):
+        state = state.capitalize()
+        browser.element('//div[text()="Select State"]').click()
+        browser.element(f'//*[text()="{state}"]').click()
+
+    def select_city(self, city):
+        city = city.capitalize()
+        browser.element('//div[text()="Select City"]').click()
+        browser.element(f'//*[text()="{city}"]').click()
+
+    def submit_button_click(self):
+        browser.element('#submit').click()
+
+    def should_registered_user_with(self, full_name, email, gender, phone, date_of_birth,
+                                    subject, hobbies, file, address, state_city):
+        browser.element('.table').all('td').even.should(
+            have.exact_texts(
+                full_name,
+                email,
+                gender,
+                phone,
+                date_of_birth,
+                subject,
+                hobbies,
+                file,
+                address,
+                state_city,
+            )
+        )
+
+        browser.element('#closeLargeModal').click()
---
+++ test_hw11.py
@@ -0,0 +1,48 @@
+import allure
+import time
+
+from Homework_11.pages.registration_page_mid import RegistrationPage
+
+
+def test_hw11():
+    registration_page = RegistrationPage()
+
+    with allure.step('Открываем форму регистрации'):
+        registration_page.open_form()
+
+    with allure.step('Ввод имени и фамилии'):
+        registration_page.fill_first_name('Timir')
+        registration_page.fill_last_name('Gazizov')
+    
+    with allure.step('Ввод электронного адреса и номер телефона'):
+        registration_page.fill_email('my_mail@gmail.com')
+        registration_page.fill_phone_number('9999999999')
+    
+    with allure.step('Выбор пола'):
+        registration_page.select_gender('male')
+    
+    with allure.step('Выбор дня рождения'):
+        registration_page.select_date_of_birth('june', 11, 1994)
+    
+    with allure.step('Выбор предметов и увлечений'):
+        registration_page.select_subject('math')
+        registration_page.select_hobbies()
+    
+    with allure.step('Загрузка фотографии'):
+        registration_page.upload_picture()
+    
+    with allure.step('Ввод адреса'):
+        registration_page.fill_address('Shevchenko street')
+        registration_page.select_state("Haryana")
+        registration_page.select_city("Karnal")
+
+    with allure.step('Нажатие "Submit"'):
+        registration_page.submit_button_click()
+
+    with allure.step('Проверка введенных данных'):
+        registration_page.should_registered_user_with('Timir Gazizov', 'my_mail@gmail.com', 'Male',
+                                                  '9999999999', '11 June,1994', 'Maths',
+                                                  'Sports, Music', 'picture.jpg', 'Shevchenko street',
+                                                  'Haryana Karnal')
+
+    time.sleep(3)
